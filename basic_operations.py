367c3b8bb05fe15945142dc27845c2e5178b6867
Anindya Das <158485024+Anindya-web@users.noreply.github.com>
2024-12-19 11:52:01 +0530

    Add files via upload

---
+++ basic_operations.py
@@ -0,0 +1,17 @@
+num_1 = float(input("Enter the value of num_1: "))
+num_2 = float(input("Enter the value of num_2: "))
+add = (num_1+num_2)
+print(f" add is equal to {add:.3f}")
+sub = (num_1-num_2)
+print(f" sub is equal to {sub:.3f}")
+mul = (num_1*num_2)
+print(f" mul is equal to {mul:.3f}")
+div = (num_1/num_2)
+print(f" div is equal to {div:.3f}")
+
+
+#Explanation:
+# This Python code snippet is performing basic arithmetic operations on two numbers provided by the user. Here's a breakdown of what each line does:
+# This line of code is prompting the user to enter a value for `num_1` by displaying the message
+# "Enter the value of num_1: ". The `input()` function takes the user's input as a string. 
+# The `float()` function then converts this input string into a floating-point number (decimal number) and assigns it to the variable `num_1`.
